dd09059bbe975a6af4e3650e667e36183e494378
Rajat <150415488+rain2108@users.noreply.github.com>
2024-03-05 00:25:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -65,6 +65,8 @@ app.get('/logout', (req, res) => {
     res.send('logged out');
 });
 
-app.listen(4002, () => {
-    console.log('listening on 4002');
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
